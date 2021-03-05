f85255009ba8ef0e747a5d3a6f9a3580ec2cd716
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-03-05 09:40:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import mysql.connector
+from flask import Flask, render_template, request
+app = Flask(__name__)
+mydb = mysql.connector.connect(
+  host="169.57.56.131",
+  port=30002,
+  user="root",
+  password="root",
+  database="MyDB"
+)
+mycursor = mydb.cursor()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        val = [(firstName, lastName)]
+        sql = "INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)"
+        mycursor.executemany(sql, val)
+        mydb.commit()
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
